df613e84c455ea884c3660a49a5e011f3eac81dd
Abhimanyu Kapur <abhimany@codeaurora.org>
2015-04-14 14:58:40 -0500

    soc: qcom: add msm8916 cpu operations
    Add qcom cpu operations for arm-v8 cpus. Implement secondary cpu boot ops
    As a part of this change update device tree documentation for:
    
    1. Arm cortex-a ACC device which provides percpu reg
    2. Armv8 cortex-a compatible string in arm/cpus.txt
    
    Signed-off-by: Abhimanyu Kapur <abhimany@codeaurora.org>
    Signed-off-by: Kumar Gala <galak@codeaurora.org>
    [minecrell: move back to drivers/soc/qcom to share between ARM32/64]

--- arch/arm64/kernel/cpu_ops.c
+++ arch/arm64/kernel/cpu_ops.c
@@ -19,12 +19,16 @@ extern const struct cpu_operations smp_spin_table_ops;
 extern const struct cpu_operations acpi_parking_protocol_ops;
 #endif
 extern const struct cpu_operations cpu_psci_ops;
+extern const struct cpu_operations qcom_cortex_a_ops;
 
 static const struct cpu_operations *cpu_ops[NR_CPUS] __ro_after_init;
 
 static const struct cpu_operations *const dt_supported_cpu_ops[] __initconst = {
 	&smp_spin_table_ops,
 	&cpu_psci_ops,
+#ifdef CONFIG_ARCH_QCOM
+	&qcom_cortex_a_ops,
+#endif
 	NULL,
 };
 
