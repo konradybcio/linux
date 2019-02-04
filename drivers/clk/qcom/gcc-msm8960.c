abc8e4eae71a0c1fd2bd5b8d8525ef75a7da2d7f
Pavel Dubrova <pashadubrova@gmail.com>
2019-02-05 01:55:13 +0200

    gcc-msm8960: comment out tsens
    Signed-off-by: Pavel Dubrova <pashadubrova@gmail.com>

--- drivers/clk/qcom/gcc-msm8960.c
+++ drivers/clk/qcom/gcc-msm8960.c
@@ -3625,7 +3625,7 @@ static int gcc_msm8960_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
 	const struct of_device_id *match;
-	struct platform_device *tsens;
+	//struct platform_device *tsens;
 	int ret;
 
 	match = of_match_device(gcc_msm8960_match_table, &pdev->dev);
@@ -3649,12 +3649,12 @@ static int gcc_msm8960_probe(struct platform_device *pdev)
 		hfpll_l2.d = &hfpll_l2_8064_data;
 	}
 
-	tsens = platform_device_register_data(&pdev->dev, "qcom-tsens", -1,
+	/*tsens = platform_device_register_data(&pdev->dev, "qcom-tsens", -1,
 					      NULL, 0);
 	if (IS_ERR(tsens))
 		return PTR_ERR(tsens);
 
-	platform_set_drvdata(pdev, tsens);
+	platform_set_drvdata(pdev, tsens);*/
 
 	return 0;
 }
