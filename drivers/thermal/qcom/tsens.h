3daf9d65bafd84859b0f20c0ba36a6583bc516d4
Konrad Dybcio <konradybcio@gmail.com>
2020-04-26 00:29:11 +0200

    thermal: qcom: tsens-v0_1: Add support for MSM8939

--- drivers/thermal/qcom/tsens.h
+++ drivers/thermal/qcom/tsens.h
@@ -505,7 +505,7 @@ irqreturn_t tsens_irq_thread(int irq, void *data);
 extern const struct tsens_plat_data data_8960;
 
 /* TSENS v0.1 targets */
-extern const struct tsens_plat_data data_8916, data_8974;
+extern const struct tsens_plat_data data_8916, data_8939, data_8974;
 
 /* TSENS v1 targets */
 extern const struct tsens_plat_data data_tsens_v1, data_8976;
