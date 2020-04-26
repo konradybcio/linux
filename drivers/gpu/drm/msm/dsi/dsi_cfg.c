89b21c065532538fabbdbcbd944f4cb808e9e1ba
Konrad Dybcio <konradybcio@gmail.com>
2020-04-26 21:21:52 +0200

    drivers: gpu: drm: Add DSI configuration for MSM8x36 and its derivatives, such as MSM8939.

--- drivers/gpu/drm/msm/dsi/dsi_cfg.c
+++ drivers/gpu/drm/msm/dsi/dsi_cfg.c
@@ -66,6 +66,26 @@ static const struct msm_dsi_config msm8916_dsi_cfg = {
 	.num_dsi = 1,
 };
 
+static const char * const dsi_8x36_bus_clk_names[] = {
+	"mdp_core", "iface", "bus",
+};
+
+static const struct msm_dsi_config msm8x36_dsi_cfg = {
+	.io_offset = DSI_6G_REG_SHIFT,
+	.reg_cfg = {
+		.num = 3,
+		.regs = {
+			{"gdsc", -1, -1},
+			{"vdda", 100000, 100},	/* 1.2 V */
+			{"vddio", 100000, 100},	/* 1.8 V */
+		},
+	},
+	.bus_clk_names = dsi_8x36_bus_clk_names,
+	.num_bus_clks = ARRAY_SIZE(dsi_8x36_bus_clk_names),
+	.io_start = { 0x1a98000, 0x1aa0000 },
+	.num_dsi = 2,
+};
+
 static const char * const dsi_8976_bus_clk_names[] = {
 	"mdp_core", "iface", "bus",
 };
@@ -235,7 +255,7 @@ static const struct msm_dsi_cfg_handler dsi_cfg_handlers[] = {
 	{MSM_DSI_VER_MAJOR_6G, MSM_DSI_6G_VER_MINOR_V1_3,
 		&msm8994_dsi_cfg, &msm_dsi_6g_host_ops},
 	{MSM_DSI_VER_MAJOR_6G, MSM_DSI_6G_VER_MINOR_V1_3_1,
-		&msm8916_dsi_cfg, &msm_dsi_6g_host_ops},
+		&msm8x36_dsi_cfg, &msm_dsi_6g_host_ops},
 	{MSM_DSI_VER_MAJOR_6G, MSM_DSI_6G_VER_MINOR_V1_4_1,
 		&msm8996_dsi_cfg, &msm_dsi_6g_host_ops},
 	{MSM_DSI_VER_MAJOR_6G, MSM_DSI_6G_VER_MINOR_V1_4_2,
