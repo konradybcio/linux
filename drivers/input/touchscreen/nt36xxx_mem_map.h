080d327086fe479bc39a777f0995787107f8fca5
LuK1337 <priv.luk@gmail.com>
2020-07-20 21:37:38 +0200

    input/touchscreen: nt36xxx: Clean up the driver
    Remove redundant functionality (wake gesture, FW flash),
    align code style with upstream.

--- drivers/input/touchscreen/nt36xxx_mem_map.h
+++ drivers/input/touchscreen/nt36xxx_mem_map.h
@@ -1,19 +1,6 @@
+// SPDX-License-Identifier: GPL-2.0-only
 /*
  * Copyright (C) 2010 - 2017 Novatek, Inc.
- *
- * $Revision: 20544 $
- * $Date: 2017-12-20 11:08:15 +0800 (週三, 20 十二月 2017) $
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful, but WITHOUT
- * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
- * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
- * more details.
- *
  */
 
 struct nvt_ts_mem_map {
